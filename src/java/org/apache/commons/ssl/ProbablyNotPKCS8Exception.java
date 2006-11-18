995c103a3be9f7bc89824bf00215138d6c7966c5
juliusd <juliusd@4e977ecf-c73e-0410-a35e-9b6747031080>
2006-11-18 04:39:49 +0000

    license info

--- src/java/org/apache/commons/ssl/ProbablyNotPKCS8Exception.java
+++ src/java/org/apache/commons/ssl/ProbablyNotPKCS8Exception.java
@@ -1,10 +1,41 @@
+/*
+ * $Header$
+ * $Revision$
+ * $Date$
+ *
+ * ====================================================================
+ *
+ *  Copyright 2006 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+
 package org.apache.commons.ssl;
 
 import java.security.GeneralSecurityException;
 
 /**
- * @author Julius Davies
- * @since 16-Nov-2006
+ * @author Credit Union Central of British Columbia
+ * @author <a href="http://www.cucbc.com/">www.cucbc.com</a>
+ * @author <a href="mailto:juliusdavies@cucbc.com">juliusdavies@cucbc.com</a>
+ * @since 16-Nov-2005
  */
 public class ProbablyNotPKCS8Exception extends GeneralSecurityException
 {
