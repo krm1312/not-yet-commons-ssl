eab21e2f6f1420ad3e5ac00669246d903dc58e76
julius <julius@4e977ecf-c73e-0410-a35e-9b6747031080>
2009-05-25 23:45:11 +0000

    regenerated some test files

--- samples/keystores/generate.sh
+++ samples/keystores/generate.sh
@@ -9,25 +9,25 @@ export VALIDITY=`echo 366 \* $EXPIRY_IN_YEARS | bc`
 
 # bouncy-castle
 export TYPE=BKS; export NAME=BC.$TYPE.ks;
-rm $NAME
+rm -f $NAME
 export CMD="$JAVA_HOME/bin/keytool -genkey -v -keyalg RSA -keysize 2048 -sigalg SHA1WithRSA -dname \"CN=$NAME, $DNAME_SUFFIX\" -validity $VALIDITY -keypass $PASS -keystore $NAME -storepass $PASS -storetype $TYPE"
 bash -c "$CMD"
 
 # SunJCE
 export TYPE=jceks; export NAME=SunJCE.$TYPE.ks;
-rm $NAME
+rm -f $NAME
 export CMD="$JAVA_HOME/bin/keytool -genkey -v -keyalg RSA -keysize 2048 -sigalg SHA1WithRSA -dname \"CN=$NAME, $DNAME_SUFFIX\" -validity $VALIDITY -keypass $PASS -keystore $NAME -storepass $PASS -storetype $TYPE"
 bash -c "$CMD"
 
 # SUN
 export TYPE=jks; export NAME=Sun.$TYPE.ks;
-rm $NAME
+rm -f $NAME
 export CMD="$JAVA_HOME/bin/keytool -genkey -v -keyalg RSA -keysize 2048 -sigalg SHA1WithRSA -dname \"CN=$NAME, $DNAME_SUFFIX\" -validity $VALIDITY -keypass $PASS -keystore $NAME -storepass $PASS -storetype $TYPE"
 bash -c "$CMD"
 
 
 # SUN with different key password
 export TYPE=jks; export NAME=Sun.2pass.$TYPE.ks;
-rm $NAME
+rm -f $NAME
 export CMD="$JAVA_HOME/bin/keytool -genkey -v -keyalg RSA -keysize 2048 -sigalg SHA1WithRSA -dname \"CN=$NAME, $DNAME_SUFFIX\" -validity $VALIDITY -keypass $KEYPASS -keystore $NAME -storepass $PASS -storetype $TYPE"
 bash -c "$CMD"
